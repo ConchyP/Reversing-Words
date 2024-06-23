4dc065b1eb03bb2160526986685bd5b5a7a2b12d
Conchy <xConchyx@gmail.com>
2024-06-23 18:15:45 +0200

    commit final

--- reverseWords.js
+++ reverseWords.js
@@ -7,8 +7,8 @@ function reverseWords (str) {
 
 let string1 = "Hello World";
 let string2 = "Hi There";
-let string3 = "I'm a coffee lover";
+let string3 = "I'm a coffee lover";  //cadena más larga para comprobar que se sigue ejecutando con más de 2 palabras
 
 console.log(reverseWords(string1));
 console.log(reverseWords(string2));
-console.log(reverseWords(string3));
+console.log(reverseWords(string3)); 
